949bd7f26bd4d39e060e775574872f2d7421c05e
jacob.holdsworth1 <jacob.holdsworth1@CSS0077>
2024-07-03 12:11:25 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace gorilla
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            GameTimer = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            player1 = new PictureBox();
+            player2 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            gameOver = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)player1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)player2).BeginInit();
+            SuspendLayout();
+            // 
+            // GameTimer
+            // 
+            GameTimer.Enabled = true;
+            GameTimer.Interval = 20;
+            GameTimer.Tick += GameTimer_Tick;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(873, 65);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(156, 151);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Black;
+            pictureBox2.Location = new Point(310, 370);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(25, 25);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // player1
+            // 
+            player1.BackColor = Color.Orange;
+            player1.Location = new Point(280, 400);
+            player1.Name = "player1";
+            player1.Size = new Size(100, 100);
+            player1.TabIndex = 3;
+            player1.TabStop = false;
+            // 
+            // player2
+            // 
+            player2.BackColor = Color.Orange;
+            player2.Location = new Point(1580, 400);
+            player2.Name = "player2";
+            player2.Size = new Size(100, 100);
+            player2.TabIndex = 3;
+            player2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(91, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 4;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BorderStyle = BorderStyle.Fixed3D;
+            label2.Location = new Point(92, 81);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 17);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
+            // 
+            // gameOver
+            // 
+            gameOver.Font = new Font("Consolas", 72F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            gameOver.Location = new Point(580, 480);
+            gameOver.Name = "gameOver";
+            gameOver.Size = new Size(740, 120);
+            gameOver.TabIndex = 6;
+            gameOver.Text = "GAME OVER";
+            gameOver.TextAlign = HorizontalAlignment.Center;
+            gameOver.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.RoyalBlue;
+            ClientSize = new Size(1904, 1041);
+            Controls.Add(gameOver);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(player2);
+            Controls.Add(player1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            SizeGripStyle = SizeGripStyle.Hide;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)player1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)player2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer GameTimer;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox player1;
+        private PictureBox player2;
+        private Label label1;
+        private Label label2;
+        private TextBox gameOver;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,227 @@
+﻿using System.Numerics;
+using System.Reflection;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace gorilla
+{
+    public partial class Form1 : Form
+    {
+
+        private Random r = new Random();
+        private Rectangle rRectangle;
+        List<Rectangle> Buildings = new List<Rectangle>();
+        List<Rectangle> Players = new List<Rectangle>();
+        List<Rectangle> PlayerBases = new List<Rectangle>();
+        int playerTurnCount = 2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += Form1_Paint;
+            this.ClientSize = new Size(1920, 1080);
+            gameOver.Visible = false;
+            CreateBuildings();
+            CreateBuildings2();
+            CreateBuildings3();
+            Player1();
+            Player2();
+            Player1Base();
+            Player2Base();
+            ThrowObject();
+            //playerCollision();
+        }
+
+        int totalwidth = 0;
+        public void GenerateRRectangle()
+        {
+            int minHeight = 500;
+            int maxHeight = 800;
+            int width = 216;
+            int rHeight = r.Next(minHeight, maxHeight + 1);
+
+            int x = (0 + totalwidth);
+            int y = (1080 - rHeight);
+
+            Rectangle rRectangle = new Rectangle(x, y, width, rHeight);
+            totalwidth = (totalwidth + 216);
+            Buildings.Add(rRectangle);
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Random rgb1 = new Random();
+            Random rgb2 = new Random();
+            Random rgb3 = new Random();
+            foreach (Rectangle r in Buildings)
+            {
+                var solidBrush = new SolidBrush(Color.FromArgb(rgb1.Next(0, 255), rgb2.Next(0, 255), rgb3.Next(0, 255)));
+                using SolidBrush brush = solidBrush;
+                e.Graphics.FillRectangle(brush, r);
+            }
+            foreach (Rectangle r in Players)
+            {
+                var solidBrush = new SolidBrush(Color.Orange);
+                using SolidBrush brush = solidBrush;
+                e.Graphics.FillRectangle(brush, r);
+            }
+            foreach (Rectangle r in PlayerBases)
+            {
+                var solidBrush = new SolidBrush(Color.Navy);
+                using SolidBrush brush = solidBrush;
+                e.Graphics.FillRectangle(brush, r);
+            }
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            GenerateRRectangle();
+            this.Invalidate();
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            label2.Text = playerTurnCount.ToString();
+        }
+
+        private void CreateBuildings()
+        {
+            while (totalwidth < 216)
+            {
+                GenerateRRectangle();
+            }
+            totalwidth = 432;
+        }
+
+        private void CreateBuildings2()
+        {
+            while (totalwidth < 1512)
+            {
+                GenerateRRectangle();
+            }
+            totalwidth = 1727;
+        }
+
+        private void CreateBuildings3()
+        {
+            while (totalwidth < 1728)
+            {
+                GenerateRRectangle();
+            }
+        }
+
+        public void Player1()
+        {
+            int playerHeight = 100;
+            int playerWidth = 100;
+
+            int x = 280;
+            int y = 400;
+
+            Rectangle Player1Rectangle = new Rectangle(x, y, playerWidth, playerHeight);
+            Players.Add(Player1Rectangle);
+        }
+
+        public void Player2()
+        {
+            int playerHeight = 100;
+            int playerWidth = 100;
+
+            int x = 1580;
+            int y = 400;
+
+            Rectangle player2 = new Rectangle(x, y, playerWidth, playerHeight);
+            Players.Add(player2);
+        }
+
+        public void Player1Base()
+        {
+            int baseHeight = 750;
+            int baseWidth = 216;
+
+            int x = 216;
+            int y = 500;
+
+            Rectangle player1base = new Rectangle(x, y, baseWidth, baseHeight);
+            PlayerBases.Add(player1base);
+        }
+
+        public void Player2Base()
+        {
+            int baseHeight = 750;
+            int baseWidth = 216;
+
+            int x = 1512;
+            int y = 500;
+
+            Rectangle player2base = new Rectangle(x, y, baseWidth, baseHeight);
+            PlayerBases.Add(player2base);
+        }
+
+        public void Throwing()
+        {
+            double g = 0;
+            double h = pictureBox2.Top;
+            double a = 5;
+            double Uv = 20;
+            double aRadian = a * Math.PI / 180.0;
+            double Ux = pictureBox2.Left;
+            double Uy = pictureBox2.Top;
+            double Sy = h + Ux * Math.Tan(aRadian) - (g * Math.Pow(Ux, 2)) / (2 * Math.Pow(Uv, 2) * Math.Pow(Math.Cos(aRadian), 2));
+            int Cy = Convert.ToInt32(Math.Round(Sy));
+            int x = Convert.ToInt32(Ux.ToString());
+            pictureBox2.Left = x++;
+            pictureBox2.Top = Cy;
+            label1.Text = (x.ToString() + ", " + Cy.ToString());
+        }
+
+        public async void ThrowObject()
+        {
+            double g = 9.8;
+            double h = pictureBox2.Top;
+            double a = 1;
+            double Uv = 1;
+
+            int totaldistance = 3200;
+            int gravity = Convert.ToInt32(g);
+            int height = Convert.ToInt32(h);
+            int angle = Convert.ToInt32(a);
+            int x_change = Convert.ToInt32(Uv);
+            int total_x_change = 0;
+
+            while (total_x_change < (totaldistance / 2))
+            {
+                pictureBox2.Left = pictureBox2.Left + x_change;
+                pictureBox2.Top = pictureBox2.Top + (angle);
+                total_x_change = total_x_change + x_change;
+                await Task.Delay(10);
+            }
+
+            while (total_x_change > (totaldistance / 2) && (total_x_change < 1920) && (pictureBox2.Top > 1000))
+            {
+                pictureBox2.Left = pictureBox2.Left + x_change;
+                pictureBox2.Top = pictureBox2.Top - (angle);
+                await Task.Delay(10);
+            }
+
+            if (total_x_change > 1920)
+            {
+                playerTurnCount = playerTurnCount++;
+            }
+        }
+
+        bool gameOn = true;
+        public async void playerCollision()
+        {
+            while (gameOn == true)
+            {
+                if (player1.Bounds.IntersectsWith(pictureBox2.Bounds) || player2.Bounds.IntersectsWith(pictureBox2.Bounds))
+                {
+                    gameOver.Visible = true;
+                    gameOn = false;
+                }
+            }
+        }
+    }
+}
